bb4ca0d90a0349fbabdbcd8d4d71cb955c0b301a
Hwisung <heesong1130@gmail.com>
2021-05-24 17:48:17 +0900

    Add : 초기세팅 완료

---
+++ src/Routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main from './Pages/Main/Main';
+
+class Routes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
