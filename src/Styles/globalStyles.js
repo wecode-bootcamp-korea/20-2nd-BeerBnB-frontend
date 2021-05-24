a88e3cdf6a383e704bc586aeb2abcc69f38f0bb5
Hwisung <heesong1130@gmail.com>
2021-05-24 21:27:57 +0900

    Modify :npm

--- src/Styles/globalStyles.js
+++ src/Styles/globalStyles.js
@@ -2,8 +2,9 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle` 
+
     ${reset}
-    
+
     a{
         text-decoration: none;
         color: inherit;
